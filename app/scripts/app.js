110b055d1e6d96dd40472d3e9a0a53824b062492
Kostas Siabanis <kostas.siabanis@gmail.com>
2017-06-26 00:42:00 +0300

    Add loader and fix a series of bugs on states/routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,19 +4,21 @@
     let genres;
 
     var s, page = 1, searchPage = 1,
-        pollingForData = false,
-        flexOrder = 1,
-        state
+        flexOrder = 1, flexOrderSearch = 1,
+        state, polling = false
         ;
 
     return {
 
         settings: {
             section: document.getElementsByTagName('section'),
+            searchSection: document.getElementById('search'),
+            nowPlayingSection: document.getElementById('now-playing'),
             navLinks: document.querySelectorAll('nav a'),
             content: document.getElementsByClassName('mdl-layout__content')[0],
             searchInput: document.getElementById('search-input'),
-            jawbones: document.getElementsByClassName('jawbone')
+            // jawbones: document.getElementsByClassName('jawbone')
+            loader: document.getElementsByClassName('loader')[0]
         },
 
         init: function () {
@@ -34,18 +36,26 @@
         window.addEventListener('hashchange', router);
         window.addEventListener('load', router);
 
-        s.content.addEventListener('scroll', function () { //window or document
+        s.content.addEventListener('scroll', function () {
+
             var reachedBottom = s.content.scrollHeight - s.content.scrollTop === s.content.clientHeight;
-            // if (!pollingForData && distToBottom > 0 && distToBottom <= 8888) {
-            if (reachedBottom) {
-                pollingForData = true;
-                page++;
-                getLatestMovies(page);
+            if (reachedBottom && !polling && s.content.scrollTop > s.content.clientHeight) {
+                if (state === 'now-playing') {
+                    page++;
+                    getLatestMovies(page);
+                }
+                else if (state === 'search') {
+                    searchPage++;
+                    searchMovies(s.searchInput.value, searchPage);
+                }
             }
         });
 
         s.searchInput.addEventListener('keyup', function (e) {
-            searchMovies(document.getElementById('search-input').value);
+            clearSearchResults();
+            if (s.searchInput.value.length > 0 && !polling) {
+                searchMovies(s.searchInput.value);
+            }
         });
 
     }
@@ -68,7 +78,9 @@
             case 'now-playing':
                 document.getElementById(state).style.display = 'flex';
                 document.querySelector('nav a[href="#' + state + '"]').classList.add('is-active');
-                getLatestMovies();
+                if (s.nowPlayingSection.querySelectorAll('.movie-card').length === 1) {
+                    getLatestMovies();
+                }
                 break;
             case 'search':
                 document.getElementById(state).style.display = 'flex';
@@ -80,7 +92,11 @@
     }
 
     function getLatestMovies(page) {
+        polling = true;
+        toggleLoader();
         dataservice.getLatestMovies(page).then(function (data) {
+            polling = false;
+            toggleLoader();
             loadMovies(data);
         })
             .catch(function (err) {
@@ -96,28 +112,27 @@
             jawbone = section.getElementsByClassName('jawbone')[0],
             jawboneClone
             ;
-        // pollingForData = false;
         data.results.map(function (movie, i) {
-            // console.log(genres);
+
             cardClone = movieCard.cloneNode(true);
             section.appendChild(cardClone);
             cardClone.setAttribute('data-id', movie.id);
             cardClone.style.display = 'flex';
-            cardClone.style.order = flexOrder;
+            cardClone.style.order = state === 'now-playing' ? flexOrder : flexOrderSearch;
             cardClone.querySelector('.movie-card__title').textContent = movie.title;
             cardClone.querySelector('.movie-card__overview').textContent = movie.overview;
             cardClone.querySelector('.movie-card__genres').textContent = movie.genre_ids;
             cardClone.querySelector('.movie-card__meta__year').textContent = movie.release_date.substring(0, 4);
             cardClone.querySelector('.movie-card__meta__rating').textContent = movie.vote_average + '/10';
-            cardClone.style.backgroundImage = 'url(http://image.tmdb.org/t/p/w300' + movie.poster_path + ')';
-            flexOrder++;
+            cardClone.style.backgroundImage = movie.poster_path ? 'url(http://image.tmdb.org/t/p/w300' + movie.poster_path + ')' : 'url(http://lorempixel.com/300/450/nightlife/)';
+            state === 'now-playing' ? flexOrder++ : flexOrderSearch++;
             cardClone.addEventListener('click', showJawbone);
 
             if ((i + 1) % 4 == 0) {
                 jawboneClone = jawbone.cloneNode(true);
                 section.appendChild(jawboneClone);
-                jawboneClone.style.order = flexOrder;
-                flexOrder++;
+                jawboneClone.style.order = state === 'now-playing' ? flexOrder : flexOrderSearch;
+                state === 'now-playing' ? flexOrder++ : flexOrderSearch++;
             }
 
         });
@@ -129,37 +144,49 @@
         })
     }
 
-    // function searchMovies(searchText) {
-    //     fetch(searchUrl + '&query=' + searchText + '&page=' + searchPage)
-    //         .then(response => response.json())
-    //         .then(function (data) {
-    //             loadSearchResults(data)
-    //         })
-    //         .catch(function (err) {
-    //             console.log('Error:' + err);
-    //         });
-    // }
+    function searchMovies(searchText, page) {
+        polling = true;
+        toggleLoader();
+        dataservice.getSearchResults(searchText, page).then(function (data) {
+            loadMovies(data);
+            polling = false;
+            toggleLoader();
+        })
+            .catch(function (err) {
+                console.log('Error:' + err);
+            });
+    }
 
-    // function loadSearchResults(data) {
-    //     // pollingForData = false;
-    //     data.results.map(function (movie) {
-    //         cardClone = document.getElementsByClassName('movie-card')[0].cloneNode(true);
-    //         document.getElementById('search').appendChild(cardClone);
-    //         cardClone.style.display = 'flex';
-    //         cardClone.querySelector('.movie-card__title').textContent = movie.title;
-    //         cardClone.style.backgroundImage = 'url(http://image.tmdb.org/t/p/w300' + movie.poster_path + ')';
-    //     });
-    // }
+    function clearSearchResults() {
+        var movieCards = s.searchSection.getElementsByClassName('movie-card');
+        var jawbones = s.searchSection.getElementsByClassName('jawbone');
 
+
+        Array.from(movieCards).map(function (card, i) {
+            if (i > 0) {
+                s.searchSection.removeChild(card);
+            }
+        });
+
+        Array.from(jawbones).map(function (jawbone, i) {
+            if (i > 0) {
+                s.searchSection.removeChild(jawbone);
+            }
+        });
+
+        flexOrderSearch = 1;
+
+    }
 
     function showJawbone() {
 
         var jawbone, jawboneOrder, movieId, movieDetails, tabsNav;
         var overviewTab, trailersTab, reviewsTab, similarTab;
         var trailers, reviews, similarMovies;
+        var activeSection = state === 'now-playing' ? s.nowPlayingSection : s.searchSection;
 
         jawboneOrder = Math.ceil(this.style.order / 5) * 5;
-        jawbone = document.querySelector('div[style*="order: ' + jawboneOrder + ';"]');
+        jawbone = activeSection.querySelector('div[style*="order: ' + jawboneOrder + ';"]');
         overviewTab = jawbone.querySelector('.js-tab-overview');
         trailersTab = jawbone.querySelector('.js-tab-trailers');
         reviewsTab = jawbone.querySelector('.js-tab-reviews');
@@ -171,11 +198,22 @@
 
 
         movieId = this.getAttribute('data-id');
+        polling = true;
+        toggleLoader();
         dataservice.getMovieDetails(movieId)
             .then(function (data) {
+                polling = false;
+                toggleLoader();
                 movieDetails = data;
                 jawbone.classList.add('is-open');
-                jawbone.style.backgroundImage = 'url(http://img.youtube.com/vi/' + movieDetails.trailers.youtube[0].source + '/maxresdefault.jpg)';
+                if (movieDetails.trailers.youtube[0]) {
+                    jawbone.style.backgroundImage = 'url(http://img.youtube.com/vi/' + movieDetails.trailers.youtube[0].source + '/maxresdefault.jpg)';
+                }
+                else {
+                    // jawbone.style.backgroundImage = 'url(http://image.tmdb.org/t/p/w300' + movieDetails.poster_path + ')';
+                    jawbone.style.backgroundImage = movieDetails.poster_path ? 'url(http://image.tmdb.org/t/p/w300' + movieDetails.poster_path + ')' : 'url(http://lorempixel.com/300/450/nightlife/)';
+                    jawbone.style.backgroundSize = 'cover';
+                }
 
                 overviewTab.querySelector('.jawbone__overview__title').textContent = movieDetails.original_title;
                 overviewTab.querySelector('.jawbone__overview__description').textContent = movieDetails.overview;
@@ -183,24 +221,24 @@
                 overviewTab.querySelector('.jawbone__overview__meta__year').textContent = movieDetails.release_date.substring(0, 4);
                 overviewTab.querySelector('.jawbone__overview__meta__rating').textContent = movieDetails.vote_average + '/10';
 
-                Array.from(trailers).map(function(trailer, i){
-                    if(movieDetails.trailers.youtube[i]) {
+                Array.from(trailers).map(function (trailer, i) {
+                    if (movieDetails.trailers.youtube[i]) {
                         trailer.querySelector('img').src = 'http://img.youtube.com/vi/' + movieDetails.trailers.youtube[i].source + '/sddefault.jpg';
                         trailer.style.display = 'block';
                         trailer.querySelector('.jawbone__trailers__trailer__title').textContent = movieDetails.trailers.youtube[i].name;
                     }
                 });
 
-                Array.from(reviews).map(function(review, i){
-                    if(movieDetails.reviews.results[i]) {
+                Array.from(reviews).map(function (review, i) {
+                    if (movieDetails.reviews.results[i]) {
                         review.style.display = 'block';
                         review.querySelector('.js-review-author').textContent = " " + movieDetails.reviews.results[i].author;
                         review.querySelector('.js-review-text').textContent = movieDetails.reviews.results[i].content;
                     }
                 });
 
-                Array.from(similarMovies).map(function(similar, i){
-                    if(movieDetails.similar.results[i]) {
+                Array.from(similarMovies).map(function (similar, i) {
+                    if (movieDetails.similar.results[i]) {
                         similar.style.display = 'block';
                         similar.style.backgroundImage = 'url(http://image.tmdb.org/t/p/w300' + movieDetails.similar.results[i].poster_path + ')';
                         similar.querySelector('.js-similar-title').textContent = movieDetails.similar.results[i].original_title;
@@ -211,14 +249,13 @@
                 });
 
 
-
             })
             .catch(function (err) {
                 console.log('Error:' + err);
             });
 
         // Close all other's
-        Array.from(s.jawbones).map(function (jawbone) {
+        Array.from(activeSection.getElementsByClassName('jawbone')).map(function (jawbone) {
             if (jawbone.style.order != jawboneOrder) {
                 jawbone.classList.remove('is-open');
             }
@@ -252,6 +289,10 @@
             tab.classList.remove('is-active');
         });
         jawbone.getElementsByClassName('js-tab-' + this.getAttribute('data-target'))[0].classList.add('is-active');
+    }
+
+    function toggleLoader() {
+        s.loader.style.display = polling ? 'flex' : 'none';
     }
 
 
--- app/scripts/dataservice.js
+++ app/scripts/dataservice.js
@@ -9,8 +9,11 @@ var dataservice = (function () {
             .then(response => response.json());
     }
 
-    function getSearchResults() {
-
+    function getSearchResults(searchText, page) {
+        page = page || 1;
+        var url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + searchText + '&page=' + page;
+        return fetch(url)
+            .then(response => response.json());
     }
 
     function getMovieDetails(movieId) {
@@ -25,50 +28,12 @@ var dataservice = (function () {
             .then(response => response.json())
     }
 
-    // var printerInstance;
-    //
-    // function create () {
-    //
-    //     function print() {
-    //         // underlying printer mechanics
-    //     }
-    //
-    //     function turnOn() {
-    //         // warm up
-    //         // check for paper
-    //     }
-    //
-    //     return {
-    //         // public + private states and behaviors
-    //         print: print,
-    //         turnOn: turnOn
-    //     };
-    // }
-    //
     return {
-        // getInstance: function() {
-        //     if(!printerInstance) {
-        //         printerInstance = create();
-        //     }
-        //     return printerInstance;
-        // }
         getLatestMovies: getLatestMovies,
         getMovieDetails: getMovieDetails,
         getSearchResults: getSearchResults,
         getGenres: getGenres
     };
-    //
-    // function Singleton () {
-    //     if(!printerInstance) {
-    //         printerInstance = intialize();
-    //     }
-    // };
+
 
 })();
-
-
-// The create method is private because we do not want the client to access this,
-// however, notice that the getInstance method is public. Each officer worker can
-// generate a printer instance by interacting with the getInstance method, like so:
-//var officePrinter = printer.getInstance();
-//var movies = dataservice.getMovies();
