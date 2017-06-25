3ce452366d4498d59b9f02bfb0127e2344399386
Kostas Siabanis <kostas.siabanis@gmail.com>
2017-06-25 19:15:39 +0300

    Fill up reviews and similar movies

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict';
 
+    let genres;
+
     var s, page = 1, searchPage = 1,
         pollingForData = false,
         flexOrder = 1,
@@ -19,7 +21,9 @@
 
         init: function () {
             s = this.settings;
+            getGenres(); //TODO: Why here? explain
             bindUIActions();
+
         }
 
     };
@@ -93,14 +97,18 @@
             jawboneClone
             ;
         // pollingForData = false;
-        // console.log(data.results);
         data.results.map(function (movie, i) {
+            // console.log(genres);
             cardClone = movieCard.cloneNode(true);
             section.appendChild(cardClone);
             cardClone.setAttribute('data-id', movie.id);
             cardClone.style.display = 'flex';
             cardClone.style.order = flexOrder;
             cardClone.querySelector('.movie-card__title').textContent = movie.title;
+            cardClone.querySelector('.movie-card__overview').textContent = movie.overview;
+            cardClone.querySelector('.movie-card__genres').textContent = movie.genre_ids;
+            cardClone.querySelector('.movie-card__meta__year').textContent = movie.release_date.substring(0, 4);
+            cardClone.querySelector('.movie-card__meta__rating').textContent = movie.vote_average + '/10';
             cardClone.style.backgroundImage = 'url(http://image.tmdb.org/t/p/w300' + movie.poster_path + ')';
             flexOrder++;
             cardClone.addEventListener('click', showJawbone);
@@ -115,43 +123,95 @@
         });
     }
 
-
-    function searchMovies(searchText) {
-        fetch(searchUrl + '&query=' + searchText + '&page=' + searchPage)
-            .then(response => response.json())
-            .then(function (data) {
-                loadSearchResults(data)
-            })
-            .catch(function (err) {
-                console.log('Error:' + err);
-            });
+    function getGenres() {
+        dataservice.getGenres().then(function (data) {
+            genres = data;
+        })
     }
 
-    function loadSearchResults(data) {
-        // pollingForData = false;
-        // console.log(data.results);
-        data.results.map(function (movie) {
-            cardClone = document.getElementsByClassName('movie-card')[0].cloneNode(true);
-            document.getElementById('search').appendChild(cardClone);
-            cardClone.style.display = 'flex';
-            cardClone.querySelector('.movie-card__title').textContent = movie.title;
-            cardClone.style.backgroundImage = 'url(http://image.tmdb.org/t/p/w300' + movie.poster_path + ')';
-        });
-    }
+    // function searchMovies(searchText) {
+    //     fetch(searchUrl + '&query=' + searchText + '&page=' + searchPage)
+    //         .then(response => response.json())
+    //         .then(function (data) {
+    //             loadSearchResults(data)
+    //         })
+    //         .catch(function (err) {
+    //             console.log('Error:' + err);
+    //         });
+    // }
+
+    // function loadSearchResults(data) {
+    //     // pollingForData = false;
+    //     data.results.map(function (movie) {
+    //         cardClone = document.getElementsByClassName('movie-card')[0].cloneNode(true);
+    //         document.getElementById('search').appendChild(cardClone);
+    //         cardClone.style.display = 'flex';
+    //         cardClone.querySelector('.movie-card__title').textContent = movie.title;
+    //         cardClone.style.backgroundImage = 'url(http://image.tmdb.org/t/p/w300' + movie.poster_path + ')';
+    //     });
+    // }
 
 
     function showJawbone() {
 
-        var jawbone, jawboneOrder, movieId, movieDetails, tabsNav, jtabs;
+        var jawbone, jawboneOrder, movieId, movieDetails, tabsNav;
+        var overviewTab, trailersTab, reviewsTab, similarTab;
+        var trailers, reviews, similarMovies;
 
         jawboneOrder = Math.ceil(this.style.order / 5) * 5;
         jawbone = document.querySelector('div[style*="order: ' + jawboneOrder + ';"]');
+        overviewTab = jawbone.querySelector('.js-tab-overview');
+        trailersTab = jawbone.querySelector('.js-tab-trailers');
+        reviewsTab = jawbone.querySelector('.js-tab-reviews');
+        similarTab = jawbone.querySelector('.js-tab-similar');
+
+        trailers = trailersTab.querySelectorAll('.jawbone__trailers__trailer');
+        reviews = reviewsTab.querySelectorAll('.js-review');
+        similarMovies = similarTab.querySelectorAll('.js-similar');
+
+
         movieId = this.getAttribute('data-id');
         dataservice.getMovieDetails(movieId)
             .then(function (data) {
                 movieDetails = data;
                 jawbone.classList.add('is-open');
                 jawbone.style.backgroundImage = 'url(http://img.youtube.com/vi/' + movieDetails.trailers.youtube[0].source + '/maxresdefault.jpg)';
+
+                overviewTab.querySelector('.jawbone__overview__title').textContent = movieDetails.original_title;
+                overviewTab.querySelector('.jawbone__overview__description').textContent = movieDetails.overview;
+                overviewTab.querySelector('.jawbone__overview__genres').textContent = movieDetails.genres;
+                overviewTab.querySelector('.jawbone__overview__meta__year').textContent = movieDetails.release_date.substring(0, 4);
+                overviewTab.querySelector('.jawbone__overview__meta__rating').textContent = movieDetails.vote_average + '/10';
+
+                Array.from(trailers).map(function(trailer, i){
+                    if(movieDetails.trailers.youtube[i]) {
+                        trailer.querySelector('img').src = 'http://img.youtube.com/vi/' + movieDetails.trailers.youtube[i].source + '/sddefault.jpg';
+                        trailer.style.display = 'block';
+                        trailer.querySelector('.jawbone__trailers__trailer__title').textContent = movieDetails.trailers.youtube[i].name;
+                    }
+                });
+
+                Array.from(reviews).map(function(review, i){
+                    if(movieDetails.reviews.results[i]) {
+                        review.style.display = 'block';
+                        review.querySelector('.js-review-author').textContent = " " + movieDetails.reviews.results[i].author;
+                        review.querySelector('.js-review-text').textContent = movieDetails.reviews.results[i].content;
+                    }
+                });
+
+                Array.from(similarMovies).map(function(similar, i){
+                    if(movieDetails.similar.results[i]) {
+                        similar.style.display = 'block';
+                        similar.style.backgroundImage = 'url(http://image.tmdb.org/t/p/w300' + movieDetails.similar.results[i].poster_path + ')';
+                        similar.querySelector('.js-similar-title').textContent = movieDetails.similar.results[i].original_title;
+                        similar.querySelector('.js-similar-year').textContent = movieDetails.similar.results[i].release_date.substring(0, 4);
+                        similar.querySelector('.js-similar-rating').textContent = movieDetails.similar.results[i].vote_average + '/10';
+                        similar.querySelector('.js-similar-overview').textContent = movieDetails.similar.results[i].overview;
+                    }
+                });
+
+
+
             })
             .catch(function (err) {
                 console.log('Error:' + err);
@@ -174,20 +234,24 @@
         });
 
         // TODO: Some elements can go to settings?
-        // tabsNav = jawbone.getElementsByClassName('js-jtabs-nav')[0];
-        // jtabs = jawbone.querySelectorAll('div[class*="js-tab-"]');
-        // Array.from(tabsNav.querySelectorAll('span')).map(function (tabNavItem) {
-        //     tabNavItem.addEventListener('click', function (e) {
-        //         Array.from(jtabs).map(function (tab) {
-        //             tab.classList.remove('is-active');
-        //         });
-        //         jawbone.getElementsByClassName('js-tab-' + this.getAttribute('data-target'))[0].classList.add('is-active');
-        //         tabsNav.getElementsByClassName('is-active')[0].classList.remove('is-active');
-        //         this.classList.add('is-active');
-        //     });
-        // });
+        tabsNav = jawbone.getElementsByClassName('js-jtabs-nav')[0];
+        Array.from(tabsNav.querySelectorAll('span')).map(function (tabNavItem) {
+            tabNavItem.addEventListener('click', showJawboneTab);
+        });
 
 
+    }
+
+    function showJawboneTab() {
+        var tabsNav = this.parentNode;
+        var jawbone = tabsNav.parentNode;
+        var jtabs = jawbone.querySelectorAll('div[class*="js-tab-"]');
+        tabsNav.querySelector('.is-active').classList.remove('is-active');
+        this.classList.add('is-active');
+        Array.from(jtabs).map(function (tab) {
+            tab.classList.remove('is-active');
+        });
+        jawbone.getElementsByClassName('js-tab-' + this.getAttribute('data-target'))[0].classList.add('is-active');
     }
 
 
--- app/scripts/dataservice.js
+++ app/scripts/dataservice.js
@@ -3,13 +3,10 @@ var dataservice = (function () {
     const API_KEY = 'bc50218d91157b1ba4f142ef7baaa6a0';
 
     function getLatestMovies(page) {
-
         page = page || 1;
         var url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + API_KEY + '&page=' + page;
-
         return fetch(url)
             .then(response => response.json());
-
     }
 
     function getSearchResults() {
@@ -17,11 +14,15 @@ var dataservice = (function () {
     }
 
     function getMovieDetails(movieId) {
-
         var url = 'https://api.themoviedb.org/3/movie/' + movieId + '?api_key=' + API_KEY + '&append_to_response=trailers,reviews,similar';
         return fetch(url)
             .then(response => response.json())
+    }
 
+    function getGenres() {
+        var url = 'https://api.themoviedb.org/3/genre/movie/list' + '?api_key=' + API_KEY;
+        return fetch(url)
+            .then(response => response.json())
     }
 
     // var printerInstance;
@@ -53,7 +54,8 @@ var dataservice = (function () {
         // }
         getLatestMovies: getLatestMovies,
         getMovieDetails: getMovieDetails,
-        getSearchResults: getSearchResults
+        getSearchResults: getSearchResults,
+        getGenres: getGenres
     };
     //
     // function Singleton () {
